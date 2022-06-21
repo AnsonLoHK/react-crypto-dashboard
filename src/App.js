4f8d4d20e99bbfd9c4467f052e8366ab03294f68
Burton <bartonchiao0724@lg-gaming.com>
2022-06-21 18:28:10 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+// v5
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { Layout, Typography, Space, Menu } from "antd";
 
-function App() {
+// const { Header, Footer, Sider, Content } = Layout;
+// const { Title, Paragraph, Text, Link } = Typography;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Menu } from "antd";
+
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  //   MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  //   MenuUnfoldOutlined,
+} from "@ant-design/icons";
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("Option 3", "3", <ContainerOutlined />),
+  getItem("Navigation One", "sub1", <MailOutlined />, [
+    getItem("Option 5", "5"),
+    getItem("Option 6", "6"),
+    getItem("Option 7", "7"),
+    getItem("Option 8", "8"),
+  ]),
+  getItem("Navigation Two", "sub2", <AppstoreOutlined />, [
+    getItem("Option 9", "9"),
+    getItem("Option 10", "10"),
+    getItem("Submenu", "sub3", null, [
+      getItem("Option 11", "11"),
+      getItem("Option 12", "12"),
+    ]),
+  ]),
+];
+
+const Navbar = () => {
+  const onClick = (e) => {
+    console.log("click ", e);
+  };
+  return (
+    <Menu
+      onClick={onClick}
+      style={{
+        width: 256,
+      }}
+      defaultSelectedKeys={["1"]}
+      defaultOpenKeys={["sub1"]}
+      mode="inline"
+      items={items}
+    />
+  );
+};
+
+export default Navbar;
