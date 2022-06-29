5f05c4f382bf9484a36d7a65bc8a1ccc647ec54e
Burton <bartonchiao0724@lg-gaming.com>
2022-06-29 18:49:25 +0800

    Cryptocurrencies.jsx 裡css 用 Ternary operator 去判斷 css

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import millify from "millify";
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ myVar }) => {
   const { data: crytpoList, isFetching } = useGetCryptosQuery({
     pollingInterval: 3000, //在使用时传入轮询间隔即可
     refetchOnMountOrArgChange: true,
@@ -16,28 +17,48 @@ const Cryptocurrencies = () => {
     setCryptos(crytpoList?.data?.coins);
   }, [crytpoList]);
 
+  const extra = (
+    <Button type="primary">
+      <PlusOutlined />
+      <span>搜索</span>
+    </Button>
+  );
+
   return (
     <>
       {/*BrowserRouter内部只能有一个根容器,包裹其他内容*/}
       {/*添加basename='/xx'后,点击Link跳转其他路由时,url会将/xx添加到路由名前,所以使用路由路径和加了cryptocurrencies的路由路径都能匹配该路由*/}
       <Router basename="/cryptocurrencies">
-        <Route path="/crypto/:id" component={CryotiDisplay}></Route>
+        <Route path="/crypto/:name" component={CryotiDisplay}></Route>
         <Row>
           {cryptos &&
             cryptos.map((crypto) => (
-              <Col key={crypto.uuid}>
+              <Col key={crypto.uuid} className="crypto-card">
                 {/* 可在对应的组件中输出props查看传入的对象的信息,添加replace将跳转前的上一个页面替换成当前页面,只将当前页面入栈*/}
                 <Link to={`/crypto/${crypto.name}`}>
-                  <Card>
+                  <Card
+                    title={extra}
+                    extra={
+                      <img className="crypto-image" src={crypto.iconUrl} />
+                    }
+                    hoverable
+                    style={{ width: 300 }}
+                  >
+                    <p>{millify(crypto.price)}</p>
                     <p
-                      style={{
-                        backgroundColor: `${crypto.color}`,
-                      }}
+                      style={
+                        `${crypto.color}` === "#000000" //黑
+                          ? { backgroundColor: "yellow" }
+                          : `${crypto.color}` === "#1b1f2a" //黑
+                          ? { backgroundColor: "green" }
+                          : `${crypto.color}` === "#3C3C3D" //黑
+                          ? { backgroundColor: "green" }
+                          : { backgroundColor: `${crypto.color}` }
+                      }
                     >
-                      {millify(crypto.price)}
+                      {millify(crypto.marketCap)}
                     </p>
-                    <p>{millify(crypto.marketCap)}</p>
-                    <p>{millify(crypto.change)}</p>
+                    <p>{millify(crypto.change)}%</p>
                     <p></p>
                   </Card>
                 </Link>
@@ -52,5 +73,6 @@ const Cryptocurrencies = () => {
 export default Cryptocurrencies;
 
 function CryotiDisplay(props) {
-  return <div>幣種:{props.match.params.id}</div>;
+  // console.log("props", props);
+  return <div>幣種:{props.match.params.name}</div>;
 }
